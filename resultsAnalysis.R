71998e7d729b217965300d360380ab56c7be4be3
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2022-10-18 14:18:44 -0300

    adding line plot

--- resultsAnalysis.R
+++ resultsAnalysis.R
@@ -10,6 +10,13 @@ if(!dir.exists(path="output")) {
 }
 
 #--------------------------------------------------------------
+# Loading auxiliary functions
+#--------------------------------------------------------------
+
+source("R/getRepResults.R")
+source("R/getAveragedResults.R")
+
+#--------------------------------------------------------------
 # Loading previous results
 #--------------------------------------------------------------
 
@@ -26,7 +33,7 @@ if(!file.exists(results.file)) {
 
 } else {
   cat(' - Results already extracted. Loading file.\n')
-  load(results.file, verbose = TRUE)
+  load(results.file, verbose = FALSE)
 }
 
 #--------------------------------------------------------------
@@ -50,21 +57,67 @@ g = g + labs(x = "Algoritmo", y = "Acurácia")
 ggsave(g, file = "plots/fig5_boxplotOverall.pdf", width = 12.2, height = 5.31)
 
 #--------------------------------------------------------------
+# Top best results
 #--------------------------------------------------------------
 
 # average results
 avg.df = getAveragedResults(all.results = all.results)
 
-# for each task, measure the rankings
+# order from best to worst
+avg.df = avg.df[order(avg.df$acc.test.mean, decreasing=TRUE),]
 
-#----------------------
+#top 20 best algorithms
+top = avg.df[1:20,]
+# > top[c(1:3,6,12)]
+#                 task              algo tuning acc.test.mean acc.test.sd
+# 117 preprocessedData   xgboost.no_norm random     0.6011318  0.05334000
+# 115     originalData   xgboost.no_norm random     0.5992799  0.05073800
+# 118     originalData xgboost.with_norm random     0.5992799  0.05073800
+# 120 preprocessedData xgboost.with_norm random     0.5992799  0.05073800
+# 43      originalData     rpart.no_norm   none     0.5974063  0.04311161
+# 45  preprocessedData     rpart.no_norm   none     0.5974063  0.04311161
+# 46      originalData   rpart.with_norm   none     0.5974063  0.04311161
+# 48  preprocessedData   rpart.with_norm   none     0.5974063  0.04311161
+# 110          pcaData       svm.no_norm random     0.5939829  0.10148995
+# 113          pcaData     svm.with_norm random     0.5939829  0.10148995
+# 103     originalData     rpart.no_norm random     0.5931780  0.03811435
+# 105 preprocessedData     rpart.no_norm random     0.5931780  0.03811435
+# 106     originalData   rpart.with_norm random     0.5931780  0.03811435
+# 108 preprocessedData   rpart.with_norm random     0.5931780  0.03811435
+# 109     originalData       svm.no_norm random     0.5809718  0.07682225
+# 111 preprocessedData       svm.no_norm random     0.5809718  0.07682225
+# 112     originalData     svm.with_norm random     0.5809718  0.07682225
+# 114 preprocessedData     svm.with_norm random     0.5809718  0.07682225
+# 55      originalData   xgboost.no_norm   none     0.5788545  0.04693872
+# 57  preprocessedData   xgboost.no_norm   none     0.5788545  0.04693872
+
+# for each task, measure the rankings
+ordered.levels = c("xgboost.no_norm", "xgboost.with_norm",
+  "rpart.no_norm", "rpart.with_norm",
+  "svm.no_norm", "svm.with_norm",
+  "C50.no_norm", "C50.with_norm",
+  "J48.no_norm", "J48.with_norm",
+  "nnet.no_norm", "nnet.with_norm",
+  "naiveBayes.no_norm", "naiveBayes.with_norm",
+  "randomForest.no_norm", "randomForest.with_norm",
+  "gausspr.no_norm", "gausspr.with_norm",
+  "kknn.no_norm", "kknn.with_norm")
+
+avg.df$algo = factor(avg.df$algo, levels = ordered.levels)
+avg.df$task = factor(avg.df$task,
+  levels = c("originalData", "preprocessedData", "pcaData"))
+
+#--------------------------------------------------------------
 # Average performance plot
-#----------------------
+#--------------------------------------------------------------
+
+g1 = ggplot(avg.df, aes(x = algo, y = acc.test.mean, group = tuning, colour = tuning))
+g1 = g1 + geom_line() + geom_point() + facet_grid(.~task)
+g1 = g1 + theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust = 1, size = 9))
+g1 = g1 + labs(x = "Algoritmo", y = "Acurácia (teste)")
+ggsave(g1, file = "fig6_performanceMedia.pdf", width = 9.75, height = 4.14)
 
 # TODO:
-# - average performance plot (line)
-# - order boxplot
-# - rankear os algoritmos
 # - olhar matrizes de confusão da arvore
 # - olhar as estatisticas das classes preditas erradamente
 # - olhar a arvore de decisão/regras
